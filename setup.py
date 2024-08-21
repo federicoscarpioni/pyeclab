fc13c53f07a48aaf3237bfd252062b7eeafc4917
= <=>
2024-08-21 15:30:07 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='pyeis',
+      version='0.1',
+      description='A Python wrapper around the EC-Lab SDK for the controll of BioLogic potentiostats.',
+      author='Federico Scarpioni',
+      author_email='federicoscarpioni@gmail.com',
+      packages=['numpy', 'matplotlib'],
+     )
