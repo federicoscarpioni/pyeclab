b1581a4dd76d9e34fb251aa2b49207ca17c8b938
Federico Scarpioni <66127194+federicoscarpioni@users.noreply.github.com>
2024-08-21 16:15:15 +0200

    Corrected name of the package on setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup(name='pyeis',
+setup(name='pyeclab',
       version='0.1',
       description='A Python wrapper around the EC-Lab SDK for the controll of BioLogic potentiostats.',
       author='Federico Scarpioni',
