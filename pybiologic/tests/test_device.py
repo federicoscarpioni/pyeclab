e1dd3b84341d7d296d29f2571bdc9fa0beb22fda
= <=>
2024-08-21 14:55:23 +0200

    Corrected
    With the proper definition of the OCV technique

--- pybiologic/tests/test_device.py
+++ pybiologic/tests/test_device.py
@@ -10,10 +10,10 @@ binary_path = "C:/EC-Lab Development Package/EC-Lab Development Package/",
 bldev = BiologicDevice(ip_address, binary_path)
 # Change  some configuration
 bldev.set_hardware_configuration(self, channel, cnx, mode)
-# Create a simple OCV sequence of 10 seconds
-...
-ocv = OCV()
-sequence_test = [ocv]
+# Create OCV technique
+ocv_params = OCV_params(10, 1, 0, 4, 0)
+ocv_technique = ocv_params.make_OCV_tech(device.is_VMP3)
+sequence_test = [ocv_technique]
 # Load to sequence on channel 1
 bldev.load_sequence(1,sequence_test)
 # Start the  technique in channel 1
