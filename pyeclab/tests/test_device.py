225c59f2fb4260852d2b79808abf8247c5eb7b33
Federico Scarpioni <66127194+federicoscarpioni@users.noreply.github.com>
2024-08-21 16:21:31 +0200

    Corrected name of the instance device for all method calls

--- pyeclab/tests/test_device.py
+++ pyeclab/tests/test_device.py
@@ -13,11 +13,11 @@ ocv_params = OCV_params(10, 1, 0, 4, 0)
 ocv_technique = ocv_params.make_OCV_tech(device.is_VMP3)
 sequence_test = [ocv_technique]
 # Load to sequence on channel 1
-bldev.load_sequence(1,sequence_test)
+device.load_sequence(1,sequence_test)
 # Start the  technique in channel 1
-bldev.start_channel(1)
+device.start_channel(1)
 # Stop manually after 8 seconds
 time.sleep(8)
-bldev.stop_channel(1)
+device.stop_channel(1)
 # Disconnect the device
-bldev.disconnect()
+device.disconnect()
