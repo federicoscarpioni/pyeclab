9bfe9edc3b1b866895c051169a48e69c3bb3e339
Jan Conen <jan.conen@ifam.fraunhofer.de>
2025-01-24 08:39:11 +0100

    make important modules available through main namespace (_init__)

---
+++ src/pyeclab/__init__.py
@@ -0,0 +1,4 @@
+from pyeclab.device import BiologicDevice
+from pyeclab.channel import Channel
+from pyeclab.techniques import CPLIM, CA_tech, CA_params, EXIT_COND
+from pyeclab.api.kbio_types import I_RANGE, E_RANGE, BANDWIDTH
