0a27247bf0958aecea47520251c75e6b671ca830
Jan Conen <jan.conen@ifam.fraunhofer.de>
2025-02-10 17:06:31 +0100

    add tests for different file modes

--- tests/test_filewriter.py
+++ tests/test_filewriter.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 import numpy as np
+import pytest
 from pyeclab.channel.writers.filewriter import FileWriter
 from pytest import fixture
 
@@ -8,6 +9,20 @@ from pytest import fixture
 @fixture
 def writer(tmp_path):
     writer = FileWriter(file_dir=tmp_path, experiment_name="Test-Experiment")
+    print(tmp_path)
+    return writer
+
+
+@fixture
+def writer_append(tmp_path):
+    writer = FileWriter(file_dir=tmp_path, experiment_name="Test-Experiment", append=True)
+    print(tmp_path)
+    return writer
+
+
+@fixture
+def writer_new_file(tmp_path):
+    writer = FileWriter(file_dir=tmp_path, experiment_name="Test-Experiment", overwrite=False)
     print(tmp_path)
     return writer
 
@@ -46,6 +61,39 @@ def test_file_header_creation(writer):
         line = f.read()
 
     assert line == "Time/s\tEwe/V\tI/A\tTechnique_num\tLoop_num\n"
+
+
+def test_file_header_creation_new_file(writer_new_file):
+    structure = ["Time/s", "Ewe/V", "I/A", "Technique_num", "Loop_num"]
+    writer_new_file.instantiate(structure)
+
+    file_path = writer_new_file.file_dir / writer_new_file.experiment_name / "measurement_data.txt"
+    with open(file_path) as f:
+        line = f.read()
+
+    assert line == "Time/s\tEwe/V\tI/A\tTechnique_num\tLoop_num\n"
+
+
+def test_file_header_creation_new_file_fail_on_existing(writer_new_file):
+    file_path = writer_new_file.file_dir / writer_new_file.experiment_name / "measurement_data.txt"
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    with open(file_path, "w") as f:
+        f.write("Something")
+
+    structure = ["Time/s", "Ewe/V", "I/A", "Technique_num", "Loop_num"]
+    with pytest.raises(FileExistsError):
+        writer_new_file.instantiate(structure)
+
+
+def test_file_header_creation_append(writer_append):
+    structure = ["Time/s", "Ewe/V", "I/A", "Technique_num", "Loop_num"]
+    writer_append.instantiate(structure)
+
+    file_path = writer_append.file_dir / writer_append.experiment_name / "measurement_data.txt"
+    with open(file_path) as f:
+        line = f.read()
+
+    assert line == ""
 
 
 @fixture
