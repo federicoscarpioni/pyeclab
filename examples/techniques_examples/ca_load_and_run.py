579d60e9ccb743845965d4247c12df2a70d6590a
= <=>
2025-02-10 17:36:44 +0100

    Create examples for CPLIM and CA

--- examples/techniques_examples/ca_load_and_run.py
+++ examples/techniques_examples/ca_load_and_run.py
@@ -1,9 +1,10 @@
 from pyeclab.device import BiologicDevice
 from pyeclab.techniques import ChronoAmperometry
 from pyeclab.channel import Channel, ChannelOptions
+import pyeclab.api.kbio_types as KBIO
 
 # IP address of the instrument
-ip_address = "172.28.26.11"
+ip_address = "172.28.20.81"
 # Path of the SDK from BioLogic installed in the machine
 binary_path = "C:/EC-Lab Development Package/EC-Lab Development Package/"
 # Istantiate device class
@@ -17,18 +18,20 @@ ca = ChronoAmperometry(device=device,
                         record_dt=1,
                         record_dI=12,
                         repeat=0,
-                        i_range='I_RANGE_100mA')
-ca_tech = ca.make_technique()
-sequence_test = [ca_tech]
+                        e_range=KBIO.E_RANGE.E_RANGE_2_5V,
+                        i_range=KBIO.I_RANGE.I_RANGE_100mA,
+                        bandwidth=KBIO.BANDWIDTH.BW_4)
+ca = ca.make_technique()
+sequence_test = [ca]
 # Istantiate channel
-test_options = ChannelOptions("2408240935_working_example_live_plot_channel")
+test_options = ChannelOptions("250210_test_cplim")
 channel1 = Channel(
     device,
     1,
-    "E:/Experimental_data/Federico/2024/python_software_test",
+    "C:/Experimental_data/Federico/2025/python_software_test",
     test_options,
-    do_live_plot=True,
-    do_print_values=False,
+    is_live_plotting=True,
+    is_printing_values= True,
 )
 channel1.load_sequence(sequence_test, ask_ok=True)
 channel1.start()
