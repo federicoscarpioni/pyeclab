1b2014612bb478bb3ac85ca946988114f6d0e8a6
= <=>
2025-02-10 17:36:25 +0100

    Refactor to new structure

--- src/pyeclab/techniques/ocv.py
+++ src/pyeclab/techniques/ocv.py
@@ -12,6 +12,7 @@ from pyeclab.device import BiologicDevice
 from pyeclab.api.kbio_api import KBIO_api
 from pyeclab.api.kbio_tech import ECC_parm, make_ecc_parm, make_ecc_parms
 
+
 @define(kw_only=True)
 class OpenCircuitVoltage:
     device: BiologicDevice
@@ -19,6 +20,8 @@ class OpenCircuitVoltage:
     record_dt: float
     e_range: KBIO.E_RANGE
     bandwidth: KBIO.BANDWIDTH
+    ecc_file : str | None = field(init = False, default = None)
+    ecc_params : list | None = field(init = False, default = None) 
 
     def make_ocv_params(self):
         # List of OCV parameters
@@ -30,25 +33,23 @@ class OpenCircuitVoltage:
         "bandwidth": ECC_parm("Bandwidth", int),
         }
 
-        p_duration = make_ecc_parm(self.device, ocv_param_names["duration"], parameters.duration)
-        p_record = make_ecc_parm(self.device, ocv_param_names["record_dt"], parameters.record_dt)
-        p_erange = make_ecc_parm(self.device, ocv_param_names["E_range"], parameters.e_range)
-        p_band = make_ecc_parm(self.device, ocv_param_names["bandwidth"], parameters.bandwidth)
-
+        p_duration = make_ecc_parm(self.device, ocv_param_names["duration"], self.duration)
+        p_record = make_ecc_parm(self.device, ocv_param_names["record_dt"], self.record_dt)
+        p_erange = make_ecc_parm(self.device, ocv_param_names["E_range"], self.e_range.value)
+        p_band = make_ecc_parm(self.device, ocv_param_names["bandwidth"], self.bandwidth.value)
+        # Make the technique parameter array
         return make_ecc_parms(self.device, p_duration, p_record, p_erange, p_band)
-        return ecc_parms_ocv
 
-    def  make_technique(self):
-
+    def choose_ecc_file(self):
         # Name of the dll for the OCV technique (for both types of instruments VMP3/VSP300)
         ocv3_tech_file = "ocv.ecc"
         ocv4_tech_file = "ocv4.ecc"
 
         # pick the correct ecc file based on the instrument family
-        tech_file_ocv = ocv3_tech_file if is_VMP3 else ocv4_tech_file
+        return ocv3_tech_file if self.device.is_VMP3 else ocv4_tech_file
 
-        # Define parameters for loading in the device using the templates
-        ecc_parms_ocv = self.make_ocv_params()
-        
-        OcvTech = namedtuple("OcvTech", "ecc_file ecc_params user_params")
-        return OcvTech(tech_file_ocv, ecc_parms_OCV, parameters)
+    def  make_technique(self):
+
+        self.ecc_file = self.choose_ecc_file()   
+        self.ecc_params = self.make_ocv_params()
+
