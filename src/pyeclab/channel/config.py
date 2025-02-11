137057ffac5835e1833fdadeea6a7074efaa61ea
Jan Conen <jan.conen@ifam.fraunhofer.de>
2025-02-11 11:01:55 +0100

    change default to match essential potentiostat capabilities

--- src/pyeclab/channel/config.py
+++ src/pyeclab/channel/config.py
@@ -3,10 +3,10 @@ from attrs import define
 
 @define
 class ChannelConfig:
-    record_ece: bool = True
-    record_charge: bool = True
+    record_ece: bool = False
+    record_charge: bool = False
     live_plot: bool = True
-    logging: bool = False
+    print_values: bool = False
     external_control: bool = False
     record_analog_In1: bool = False
     record_analog_In2: bool = False
