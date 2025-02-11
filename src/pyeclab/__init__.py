a78014e28318d3efbd871a041477840913c6495b
Jan Conen <jan.conen@ifam.fraunhofer.de>
2025-02-11 10:43:06 +0100

    change imports to match new structure

--- src/pyeclab/__init__.py
+++ src/pyeclab/__init__.py
@@ -1,4 +1,4 @@
 from pyeclab.device import BiologicDevice
-from pyeclab.channel import Channel, ChannelOptions
-from pyeclab.techniques import OpenCircuitVoltage ,ChronoPotentiometryWithLimits, ChronoAmperometry, Loop 
+from pyeclab.channel import Channel
 from pyeclab.api.kbio_types import I_RANGE, E_RANGE, BANDWIDTH
+from pyeclab.techniques.exit_cond import EXIT_COND
