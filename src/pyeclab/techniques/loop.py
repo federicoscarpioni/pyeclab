4423eb96072810d000b46e0f5128382d40754e21
Jan Conen <jan.conen@ifam.fraunhofer.de>
2025-02-11 16:10:47 +0100

    modify to fix issues on parameters chosen

--- src/pyeclab/techniques/loop.py
+++ src/pyeclab/techniques/loop.py
@@ -13,14 +13,14 @@ from pyeclab.api.kbio_tech import ECC_parm, make_ecc_parm, make_ecc_parms
 
 # LoopTech = namedtuple("LoopTech", "ecc_file ecc_params user_params")
 
+
 @define(kw_only=True)
 class Loop:
     device: BiologicDevice
     repeat_N: int
     loop_start: int
-    ecc_file : str | None = field(init = False, default = None)
-    ecc_params : list | None = field(init = False, default = None)
-
+    ecc_file: str | None = field(init=False, default=None)
+    ecc_params: KBIO.EccParams | None = field(init=False, default=None)
 
     def make_loop_params(self):
         # Dictionary of parameters used to call the labrary later
@@ -29,21 +29,20 @@ class Loop:
             "loop_start": ECC_parm("protocol_number", int),
         }
 
-        p_repeat_N = make_ecc_parm(self.device, loop_parms["reapeat"], parameters.repeat_N)
-        p_loop_start = make_ecc_parm(self.device, loop_parms["loop_start"], parameters.loop_start)
+        p_repeat_N = make_ecc_parm(self.device, loop_parms["reapeat"], self.repeat_N)
+        p_loop_start = make_ecc_parm(self.device, loop_parms["loop_start"], self.loop_start)
 
         ecc_parms_loop = make_ecc_parms(self.device, p_repeat_N, p_loop_start)
         return ecc_parms_loop
-    
+
     def choose_ecc_file(self):
-         # .ecc file names
+        # .ecc file names
         loop3_tech_file = "loop.ecc"
         loop4_tech_file = "loop4.ecc"
 
         # pick the correct ecc file based on the instrument family
-        return loop3_tech_file if is_VMP3 else loop4_tech_file
-
+        return loop3_tech_file if self.device.is_VMP3 else loop4_tech_file
 
     def make_technique(self):
-        self.ecc_file = self.choose_ecc_file()   
+        self.ecc_file = self.choose_ecc_file()
         self.ecc_params = self.make_loop_params()
