aec33544f41f3aff742fba063cf20b22a10f898d
= <=>
2025-02-25 13:30:27 +0100

    Update examples for all functinalities
    Still to be tested on the real device!

--- examples/techniques/ca_load_and_run.py
+++ examples/techniques/ca_load_and_run.py
@@ -3,39 +3,35 @@ from pyeclab.techniques import ChronoAmperometry
 from pyeclab.channel import Channel, ChannelOptions
 import pyeclab.api.kbio_types as KBIO
 
+# IP address of the instrument
 ip_address = "172.28.20.81"
+# Path of the SDK from BioLogic installed in the machine
 binary_path = "C:/EC-Lab Development Package/EC-Lab Development Package/"
+# Istantiate device class
 device = BiologicDevice(ip_address, binary_path=binary_path)
-
-ca = ChronoAmperometry(
-    device=device,
-    voltage=0,
-    duration=10,
-    vs_init=True,
-    nb_steps=0,
-    record_dt=1,
-    record_dI=12,
-    repeat=0,
-    e_range=KBIO.E_RANGE.E_RANGE_2_5V,
-    i_range=KBIO.I_RANGE.I_RANGE_100mA,
-    bandwidth=KBIO.BANDWIDTH.BW_4
-)
+# Create OCV technique
+ca = ChronoAmperometry(device=device,
+                        voltage=0,
+                        duration=10,
+                        vs_init=True,
+                        nb_steps=0,
+                        record_dt=1,
+                        record_dI=12,
+                        repeat=0,
+                        e_range=KBIO.E_RANGE.E_RANGE_2_5V,
+                        i_range=KBIO.I_RANGE.I_RANGE_100mA,
+                        bandwidth=KBIO.BANDWIDTH.BW_4)
 ca = ca.make_technique()
-
-sequence = [ca]
-
-writer = FileWriter(
-    file_dir=Path("C:/Users/385-lab/Desktop/data/"),
-    experiment_name="2025-02-19_Test_02",
-)
-
+sequence_test = [ca]
+# Istantiate channel
+test_options = ChannelOptions("250210_test_cplim")
 channel1 = Channel(
     device,
     1,
-    writer=writer,
-    config=ChannelConfig(live_plot=False),
+    "C:/Experimental_data/Federico/2025/python_software_test",
+    test_options,
+    is_live_plotting=True,
+    is_printing_values= True,
 )
-
-channel1.load_sequence(sequence)
-
+channel1.load_sequence(sequence_test, ask_ok=True)
 channel1.start()
--- examples/techniques/cplim_load_and_run.py
+++ examples/techniques/cplim_load_and_run.py
@@ -1,43 +1,39 @@
 from pyeclab.device import BiologicDevice
-from pyeclab.techniques import ChronoPotentiometryWithLimits, build_limit
-from pyeclab.channel import Channel
-from pyeclab.channel.config import ChannelConfig
+from pyeclab.techniques import ChronoPotentiometryWithLimits
+from pyeclab.channel import Channel, ChannelOptions
 import pyeclab.api.kbio_types as KBIO
 
+# IP address of the instrument
 ip_address = "172.28.20.81"
+# Path of the SDK from BioLogic installed in the machine
 binary_path = "C:/EC-Lab Development Package/EC-Lab Development Package/"
+# Istantiate device class
 device = BiologicDevice(ip_address, binary_path=binary_path)
-
-cplim = ChronoPotentiometryWithLimits(    
-    device=device,
-    current=0.001,
-    duration=10,
-    vs_init=False,
-    nb_steps=0,
-    record_dt=1,
-    record_dE=0.1,
-    repeat=0,
-    i_range=I_RANGE.I_RANGE_100mA,
-    e_range=E_RANGE.E_RANGE_2_5V,
-    exit_cond=EXIT_COND.NEXT_TECHNIQUE,
-    limit_variable=build_limit("voltage", "greater", "or", True),
-    limit_values=3, 
-    bandwidth=BANDWIDTH.BW_5,
-)
-
+# Create OCV technique
+cplim = ChronoPotentiometryWithLimits(device=device,
+                                      current=0.001,
+                                      duration=10,
+                                      vs_init=False,
+                                      nb_steps=0,
+                                      record_dt=1,
+                                      record_dE=12,
+                                      repeat=0,
+                                      limit_variable=0b011101,
+                                      limit_values=10,
+                                      e_range=KBIO.E_RANGE.E_RANGE_2_5V,
+                                      i_range=KBIO.I_RANGE.I_RANGE_100mA,
+                                      bandwidth=KBIO.BANDWIDTH.BW_4)
 cplim.make_technique()
-
-sequence = [cplim]
-
-writer = FileWriter(
-    file_dir=Path("C:/Users/385-lab/Desktop/data/"),
-    experiment_name="2025-02-19_Test_02",
-)
+sequence_test = [cplim]
+# Istantiate channel
+test_options = ChannelOptions("250210_test_cplim")
 channel1 = Channel(
     device,
     1,
-    writer=writer,
-    config=ChannelConfig(live_plot=False),
+    "C:/Experimental_data/Federico/2025/python_software_test",
+    test_options,
+    is_live_plotting=True,
+    is_printing_values= True
 )
-channel1.load_sequence(sequence)
+channel1.load_sequence(sequence_test, ask_ok=True)
 channel1.start()
