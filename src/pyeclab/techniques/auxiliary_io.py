618f652fc32d30abe3567a59abaf7fc752c25c38
Federico Scarpioni <federicoscarpioni@gmail.com>
2025-03-10 14:41:21 +0100

    Corrected syntax for function

--- src/pyeclab/techniques/auxiliary_io.py
+++ src/pyeclab/techniques/auxiliary_io.py
@@ -1,11 +1,13 @@
-def generate_xctr_param():
+from pyeclab.channel.config import ChannelConfig
+
+def generate_xctr_param(config:ChannelConfig):
     bitfield = 0
-    bitfield |= self.config.record_ece << 0  # Record Ece at bit position 1
-    bitfield |= self.config.record_analog_In1 << 1  # Record Analog IN1 at bit position 2
-    bitfield |= self.config.record_analog_In2 << 2  # Record Analog IN2 at bit position 3
-    bitfield |= self.config.external_control << 3  # Enable External ctrl at bit position 4
+    bitfield |= config.record_ece << 0  # Record Ece at bit position 1
+    bitfield |= config.record_analog_In1 << 1  # Record Analog IN1 at bit position 2
+    bitfield |= config.record_analog_In2 << 2  # Record Analog IN2 at bit position 3
+    bitfield |= config.external_control << 3  # Enable External ctrl at bit position 4
     # bit 5 is reserved
     # No information for bit position 6 (Record Control), assuming not needed
-    bitfield |= self.config.record_charge << 6  # Record Charge at bit position 7
+    bitfield |= config.record_charge << 6  # Record Charge at bit position 7
     # No information for bit position 8 (Record IRange), assuming not needed
     return bitfield
